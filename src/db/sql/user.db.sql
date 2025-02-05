c3c45a94aafe015d8e0a0fded079f774aff70b5b
yoon-H <ucando0320@gmail.com>
2025-02-05 17:09:45 +0900

    fix : fix user id to snake case

--- src/db/sql/user.db.sql
+++ src/db/sql/user.db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    userId          INT AUTO_INCREMENT PRIMARY KEY,
+    user_id          INT AUTO_INCREMENT PRIMARY KEY,
     name            VARCHAR(255) UNIQUE,
     email           VARCHAR(255) UNIQUE,
     password        VARCHAR(255),
