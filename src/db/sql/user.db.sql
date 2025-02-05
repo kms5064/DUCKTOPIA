9e5a59e796b4589b819105e7f578a292646c0bbb
yoon-H <ucando0320@gmail.com>
2025-02-05 14:16:44 +0900

    add : add user.db.sql

---
+++ src/db/sql/user.db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    userId          INT AUTO_INCREMENT PRIMARY KEY,
+    name            VARCHAR(255) UNIQUE,
+    email           VARCHAR(255) UNIQUE,
+    password        VARCHAR(255),
+    is_logged_in    BOOL DEFAULT 0,
+    create_dt       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    update_dt       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
