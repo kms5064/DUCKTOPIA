f3c2d748eb4e4531f6b4b467e160be1dbe1e62cc
yoon-H <ucando0320@gmail.com>
2025-02-05 15:06:55 +0900

    add : createSchemas.js

--- src/handlers/user/signUp.handler.js
+++ src/handlers/user/signUp.handler.js
@@ -1,25 +1,21 @@
 const signUpHandler = async (socket, payload) => {
-    const {id, password, email, name} = payload;
+  const { email, password, name } = payload;
 
-    // 1. payload 유효성 검사(TODO 클라 보고 고치기!)
+  // 1. payload 유효성 검사 TODO
 
-    
+  // 2. 유저 DB 확인 (TODO DB 함수 만들고 변경)
 
-    // 2. 유저 DB 확인 (TODO DB 함수 만들고 변경)
+  // 3. 비밀번호 bcrypt로 암호화 (TODO 변겨어엉)
 
-    // 3. 비밀번호 bcrypt로 암호화 (TODO 변겨어엉)
+  // 4. 유저 정보 저장 (TODO 크리에이뜨 유절)
 
-    // 4. 유저 정보 저장 (TODO 크리에이뜨 유절)
+  //
+  let user = await findUserByDeviceID(deviceId);
 
-    // 
-    let user = await findUserByDeviceID(deviceId);
-
-    // 유저가 이미 존재하는지 확인
-    if (!user) {
-      user = await createUser(deviceId);
-    } else {
-      await updateUserLogin(user.id);
-    }
-
-
-}
+  // 유저가 이미 존재하는지 확인
+  if (!user) {
+    user = await createUser(deviceId);
+  } else {
+    await updateUserLogin(user.id);
+  }
+};
