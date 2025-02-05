184bcaf4e127cbac8e440b8979dc9a284e380e09
yoon-H <ucando0320@gmail.com>
2025-02-05 17:23:56 +0900

    fix : fix db sql

--- src/db/sql/user.db.sql
+++ src/db/sql/user.db.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    user_id          INT AUTO_INCREMENT PRIMARY KEY,
+    user_id         INT AUTO_INCREMENT PRIMARY KEY,
     name            VARCHAR(255) UNIQUE,
     email           VARCHAR(255) UNIQUE,
     password        VARCHAR(255),
     is_logged_in    BOOL DEFAULT 0,
     create_dt       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    update_dt       TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    update_dt       TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
--- src/db/user/user.queries.js
+++ src/db/user/user.queries.js
@@ -1,8 +1,8 @@
 const SQL_USER_QUERIES = {
   FIND_USER_BY_EMAIL:
-    'SELECT userId, name, email, password, isLogin, create_dt, update_dt FROM user WHERE email = ?',
-  CREATE_USER: 'INSERT INTO user (name, email, password) VALUES (?,?,?)',
-  UPDATE_USER_LOGIN: 'UPDATE user SET isLogin = ?, updatd_dt = ? WHERE email = ?',
+    'SELECT userId, name, email, password, isLogin, create_dt, update_dt FROM users WHERE email = ?',
+  CREATE_USER: 'INSERT INTO users (name, email, password) VALUES (?,?,?)',
+  UPDATE_USER_LOGIN: 'UPDATE users SET isLogin = ? WHERE email = ?',
 };
 
 export default SQL_USER_QUERIES;
