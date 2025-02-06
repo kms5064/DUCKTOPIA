c4ee38ec69d6c1e187084444ccf6c0de5abf680e
yoon-H <ucando0320@gmail.com>
2025-02-06 11:05:34 +0900

    fix : fix names

--- src/constants/env.js
+++ src/constants/env.js
@@ -6,8 +6,8 @@ export const PORT = process.env.PORT || 5555;
 export const HOST = process.env.HOST || 'localhost';
 export const CLIENT_VERSION = process.env.CLIENT_VERSION || '1.0.0';
 
-export const DB_NAME = process.env.DB1_NAME || 'database';
-export const DB_USER = process.env.DB1_USER || 'user';
-export const DB_PASSWORD = process.env.DB1_PASSWORD || 'password';
-export const DB_HOST = process.env.DB1_HOST || 'localhost';
-export const DB_PORT = process.env.DB1_PORT || 3306;
+export const DB_NAME = process.env.DB_NAME || 'database';
+export const DB_USER = process.env.DB_USER || 'user';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'password';
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_PORT = process.env.DB_PORT || 3306;
