6d1b5919c9956168cb6e151c718fcf526b19f1c7
yoon-H <ucando0320@gmail.com>
2025-02-08 00:31:45 +0900

    fix: fix getRoomList response

--- src/handlers/room/getRoomList.handler.js
+++ src/handlers/room/getRoomList.handler.js
@@ -3,9 +3,7 @@ import { roomSession } from '../../sessions/session.js';
 const getRoomListHandler = (socket, payload) => {
   // 1. 패킷 전송
   const getRoomListResponse = makePacket(PACKET_TYPE.GET_ROOM_LIST_RESPONSE, {
-    success: true,
     rooms: roomSession.getRoomDatas(),
-    message: '방 리스트를 조회했습니다!',
   });
 
   socket.write(getRoomListResponse);
