c36bd18dec00f1550cce6926c17ca07f36a7237c
yoon-H <ucando0320@gmail.com>
2025-02-07 17:46:05 +0900

    add: add getRoomListHandler

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,6 +1,7 @@
 import { config } from '../config/config.js';
 import attackPlayerHandler from './player/attackPlayer.handler.js';
 import createRoomHandler from './room/createRoom.handler.js';
+import getRoomListHandler from './room/getRoomList.handler.js';
 import signInHandler from './user/signIn.handler.js';
 import signUpHandler from './user/signUp.handler.js';
 
@@ -9,6 +10,7 @@ const handlers = {
   [config.packetType.LOGIN_REQUEST[0]]: signInHandler,
   [config.packetType.PLAYER_ATTACK_REQUEST[0]]: attackPlayerHandler,
   [config.packetType.CREATE_ROOM_REQUEST[0]]: createRoomHandler,
+  [config.packetType.GET_ROOM_LIST_REQUEST[0]]: getRoomListHandler,
 };
 
 export default handlers;
---
+++ src/handlers/room/getRoomList.handler.js
@@ -0,0 +1,36 @@
+import { roomSession } from '../../sessions/session.js';
+
+const getRoomListHandler = (socket, payload) => {
+  // 1.현재 rooms에서 roomDatas로 가공
+  const rooms = roomSession.getRooms();
+
+  let roomDatas = [];
+
+  for (const room of rooms) {
+    const roomUsers = room.getUsers();
+
+    // 유저 이름 추출
+    let users = [];
+    for (const user of roomUsers) {
+      users.push(user.name);
+    }
+
+    // roomId, state, users 추출
+    roomDatas.push({
+      roomId: room.id,
+      state: room.state,
+      users,
+    });
+  }
+
+  // 2. 패킷 전송
+  const getRoomListResponse = makePacket(PACKET_TYPE.GET_ROOM_LIST_RESPONSE, {
+    success: true,
+    rooms: roomDatas,
+    message: '방 리스트를 조회했습니다!',
+  });
+
+  socket.write(getRoomListResponse);
+};
+
+export default getRoomListHandler;
