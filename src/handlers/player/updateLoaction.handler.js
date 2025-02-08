22d6d230d8be652438489381b5ddf741a7f250eb
Youmin Lee <youmin0107@naver.com>
2025-02-08 19:18:56 +0900

    update: updateLocationHandler
    플레이어 이동 동기화 틀 완성

--- src/handlers/player/updateLoaction.handler.js
+++ src/handlers/player/updateLoaction.handler.js
@@ -1,3 +1,6 @@
+import { LOCATION_REQ_TIME_TERM,VALID_DISTANCE } from "../../constants/player.js";
+
+
 const updateLocationHandler = ({ socket, payload }) => {
   try {
     const { x,y } = payload;
@@ -15,17 +18,28 @@ const updateLocationHandler = ({ socket, payload }) => {
     }
     //레이턴시 구하기
     player.latency = Date.now() - player.lastPosUpdateTime;
+    player.lastPosUpdateTime = Date.now();
 
     // 현재 위치와 요청받은 위치로 방향을 구하고 speed와 레이턴시를 곱해 이동거리를 구하고 좌표 예측 검증
+    const ceta = (Math.atan2(y-player.y, x-player.x) * 180) / Math.PI;
+    const distance = player.speed * player.latency;
+    
     // 만약 거속시로 구한 거리보다 멀면 서버가 알고있는 좌표로 강제 이동
+    if(distance>VALID_DISTANCE){
+      console.error(`유효하지 않은 이동입니다.`);
+    }
+
+    const targetX = Math.cos(ceta) * distance;
+    const targetY = Math.sin(ceta) * distance;
+
     // 유효한 이동이라면 player.lastPosUpdateTime 업데이트
+    player.x = targetX;
+    player.y = targetY;
+
     // 계산한 좌표 전송(브로드캐스트)
+    const packet = {x:player.x,y:player.y};
 
-
-
-    const packet = 1;
-
-    socket.write(packet);
+    socket.broadcast(packet);
   } catch (error) {
     handleError(socket, error);
   }
