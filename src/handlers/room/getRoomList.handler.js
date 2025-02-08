0038afa2d33143b023935a9e9ca6325f67002a7a
yoon-H <ucando0320@gmail.com>
2025-02-08 18:17:35 +0900

    bugfix: import PACKET TYPE in getRoomList Handler

--- src/handlers/room/getRoomList.handler.js
+++ src/handlers/room/getRoomList.handler.js
@@ -1,7 +1,8 @@
 import { roomSession } from '../../sessions/session.js';
 import makePacket from '../../utils/packet/makePacket.js';
+import { PACKET_TYPE } from '../../config/constants/header.js';
 
-const getRoomListHandler = ({socket, payload}) => {
+const getRoomListHandler = ({ socket, payload }) => {
   // 1. 패킷 전송
   const getRoomListResponse = makePacket(PACKET_TYPE.GET_ROOM_LIST_RESPONSE, {
     rooms: roomSession.getRoomsData(),
