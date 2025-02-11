435f17eb3b9d0ab405b19e608ad2ff49b92a4137
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-02-11 19:20:13 +0900

    refactor: startGame 핸들러 리팩토링
    config 사용 및 broadcast 메서드 사용

--- src/handlers/game/startGame.handler.js
+++ src/handlers/game/startGame.handler.js
@@ -1,41 +1,6 @@
-// import Game from '../../class/game/game.class.js';
-// import Player from '../../class/player/player.class.js';
-import { PACKET_TYPE } from '../../config/constants/header.js';
+import { config } from '../../config/config.js';
 import { roomSession, userSession } from '../../sessions/session.js';
 import makePacket from '../../utils/packet/makePacket.js';
-
-// const gamePrepareReqHandler = (room) => {
-//   try {
-//     const game = new Game(room.uuid); //room의 uuid 따라가기
-
-//     room.users.forEach((user) => {
-//       const player = new Player(id, x, y, user);
-//       game.addPlayer(player);
-//     });
-
-//     //map정보 넣기
-//     //base좌표 넣기
-
-//     const payload = {
-//       S2CGamePrepareResponse: {
-//         success,
-//       },
-//     };
-
-//     const message = dataType.create(payload);
-//     const S2CGamePrepareResponse = dataType.encode(message).finish();
-
-//     const notification = payloadParser(S2CGamePrepareResponse);
-
-//     game.players.forEach((player) => {
-//       player.socket.write(notification);
-//     });
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
-
-// export default gamePrepareReqHandler;
 
 const gameStartHandler = ({socket, payload}) => {
   try {
@@ -46,16 +11,13 @@ const gameStartHandler = ({socket, payload}) => {
       throw new Error('방 생성에 실패했습니다!');
     }
     
-    const GameStartNotification = makePacket(PACKET_TYPE.START_GAME_NOTIFICATION, {
+    const GameStartNotification = makePacket(config.packetType.START_GAME_NOTIFICATION, {
       gameState: { phaseType: 1, nextPhaseAt: 100000 }, //이삭님 코드에 이렇게돼있음!
-      users: room.getUsersData(),
+      users: room.getUserData(),
       characterPositions: room.getUsersPositionData()
     })
 
-    room.joinUserNotification(GameStartNotification);
-
-    console.log('Game Start!');
-
+    room.broadcast(GameStartNotification);
   } catch (err) {
 
   }
