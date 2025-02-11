232f3d2b314085b042f0b76623c6158f7b2904d8
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-02-11 14:44:31 +0900

    fix: 미사용 import 제거

--- src/sessions/session.js
+++ src/sessions/session.js
@@ -1,6 +1,5 @@
 import UserSession from '../classes/user/userSession.class.js';
 import RoomSession from '../classes/room/roomSession.class.js';
-import LoginSession from '../classes/user/loginSession.class.js';
 
 export const userSession = new UserSession();
 export const roomSession = new RoomSession();
