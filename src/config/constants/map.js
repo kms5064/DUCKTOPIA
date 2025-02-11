5b11cb5a28803db48973bc82691663d585118602
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-02-11 16:26:27 +0900

    move: 가독성을 위해 코드 위치 변경

--- src/config/constants/map.js
+++ src/config/constants/map.js
@@ -1,4 +1,4 @@
 export const MAX_VALUE_X = 2000; // 최대 X 좌표 값
-export const MIN_VALUE_X = -2000; // 최소 X 좌표 값
 export const MAX_VALUE_Y = 1000; // 최대 Y 좌표 값
+export const MIN_VALUE_X = -2000; // 최소 X 좌표 값
 export const MIN_VALUE_Y = -1000; // 최소 Y 좌표 값
