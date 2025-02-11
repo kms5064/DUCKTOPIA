cb1546880934551dea9fd6ae0a6303c42cbf6d31
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-02-11 14:12:24 +0900

    refactor: 환경변수 및 상수 구조 리팩토링
    기존 constants 폴더가 2개 있는 경우와
    config를 거치지 않고 직접 임포트하는 구조를 수정하였음

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,3 @@
-import { version } from 'uuid';
 import {
   CLIENT_VERSION,
   DB_HOST,
@@ -8,6 +7,7 @@ import {
   DB_USER,
   HOST,
   PORT,
+  LOCATION_REQ_TIME_TERM,
 } from './constants/env.js';
 import {
   PACKET_TYPE,
@@ -15,7 +15,11 @@ import {
   PAYLOAD_LENGTH_BYTE,
   VERSION_LENGTH_BYTE,
 } from './constants/header.js';
-import { CORE_MAX_HP } from './constants/core.js';
+import { CORE_MAX_HP, CORE_POSITION } from './constants/core.js';
+import { ATK_PER_LV, PLAYER_DEFAULT_ANGLE, PLAYER_DEFAULT_RANGE } from './constants/player.js';
+import { PLAYER_MAX_HUNGER } from '../constants/player.js';
+import { MAX_DROP_ITEM_COUNT, MAX_SPAWN_COUNT } from './constants/monster.js';
+import { MAX_VALUE_X, MAX_VALUE_Y, MIN_VALUE_X, MIN_VALUE_Y } from './constants/map.js';
 
 export const config = {
   header: {
@@ -32,6 +36,7 @@ export const config = {
   },
   client: {
     version: CLIENT_VERSION,
+    locationReqTimeTerm: LOCATION_REQ_TIME_TERM,
   },
   databases: {
     USER_DB: {
@@ -44,7 +49,27 @@ export const config = {
   },
   game : {
     core: {
-      maxHP: CORE_MAX_HP
+      maxHP: CORE_MAX_HP,
+      position: CORE_POSITION,
+    },
+    player:{
+      atkPerLv: ATK_PER_LV,
+      playerMaxHunger: PLAYER_MAX_HUNGER,
+      playerDefaultRange: PLAYER_DEFAULT_RANGE,
+      playerDefaultAngle: PLAYER_DEFAULT_ANGLE,
+      playerMaxHealth: PLAYER_MAX_HP,
+      playerSpeed: PLAYER_SPEED,
+      validDistance: VALID_DISTANCE,
+    },
+    monster:{
+      maxItemDropCount: MAX_DROP_ITEM_COUNT,
+      maxSpawnCount: MAX_SPAWN_COUNT,
+    },
+    map: {
+      startX: MIN_VALUE_X,
+      startY: MIN_VALUE_Y,
+      endX: MAX_VALUE_X,
+      endY: MAX_VALUE_Y,
     }
   }
 };
