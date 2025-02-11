8fb73bcf44c92fdff2f21f92afa6843d565e4289
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-02-11 14:21:53 +0900

    refactor: 로그인 핸들러 리팩토링
    재사용성 없는 파일의 경우 utils 대신 핸들러 내부에 위치하도록 변경
    
    디버깅을 위한 콘솔은 임시 주석처리
    
    추가로 Error 의 경우 console.log가 중복이기에 제거

--- src/handlers/user/signUp.handler.js
+++ src/handlers/user/signUp.handler.js
@@ -1,20 +1,35 @@
 import bcrypt from 'bcrypt';
+import Joi from 'joi';
 import { createUser } from '../../db/user/user.db.js';
-import { signUpSchema } from '../../utils/validations/auth.validation.js';
-import makePacket from '../../utils/packet/makePacket.js';
 import { PACKET_TYPE } from '../../config/constants/header.js';
+import makePacket from '../../utils/packet/makePacket.js';
 import { errorHandler } from '../../utils/error/errorHandler.js';
 
 const SALT_OR_ROUNDS = 10;
 
+const signUpSchema = Joi.object({
+  name: Joi.string().min(2).max(20).required().messages({
+    'string.min': '닉네임은 2글자 이상이어야 합니다.',
+    'string.max': '닉네임은 20글자를 넘길 수 없습니다.',
+    'any.required': '닉네임을 다시 입력해주세요.',
+  }),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .required()
+    .messages({
+      'string.email': '이메일 형식에 적합하지 않습니다.',
+      'any.required': '이메일을 다시 입력해주세요',
+    }),
+  password: Joi.string().min(6).max(15).required().messages({
+    'string.min': '비밀번호는 6글자 이상이어야 합니다.',
+    'string.max': '비밀번호는 15글자를 넘길 수 없습니다.',
+    'any.required': '비밀번호를 다시 입력해주세요.',
+  }),
+});
+
 const signUpHandler = async ({ socket, payload }) => {
   try {
     const { email, password, name } = payload;
-
-    /** 테스트 데이터 */
-    // const name = 'qwer';
-    // const email = 'asdf@naver.com';
-    // const password = 'asdfasdf';
 
     const obj = { name, email, password };
 
@@ -27,16 +42,13 @@ const signUpHandler = async ({ socket, payload }) => {
     // 3. 유저 정보 저장
     await createUser(name, email, hashedPw);
 
-    console.log('회원가입 성공!');
+    // console.log('회원가입 성공!');
 
     // 4. 패킷 전송
     const registerResponse = makePacket(PACKET_TYPE.REGISTER_RESPONSE, { success: true });
 
     socket.write(registerResponse);
   } catch (error) {
-    console.log(error.code);
-    console.log(error);
-
     errorHandler(socket, error);
   }
 };
