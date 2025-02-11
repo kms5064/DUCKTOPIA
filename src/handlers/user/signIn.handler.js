1019b61e732611f5561ed78663f13ca6f999dfd9
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-02-11 14:43:25 +0900

    refactor: 로그인 핸들러 수정
    기론 로직 리팩토링 과정에서 forEach 대신 map 객체로 중복로그인을 확인하는 방식으로 변경

--- src/handlers/user/signIn.handler.js
+++ src/handlers/user/signIn.handler.js
@@ -6,15 +6,12 @@ import { errorHandler } from '../../utils/error/errorHandler.js';
 import CustomError from '../../utils/error/customError.js';
 import bcrypt from 'bcrypt';
 
-let newId = 1; // (TODO) 임시로!!
-
 const signInHandler = async ({ socket, payload }) => {
   try {
     const { email, password } = payload;
 
     // 1. 사용자 존재 여부 DB에서 확인
     const userData = await findUserByEmail(email);
-
     if (!userData) {
       throw new CustomError('아이디 또는 비밀번호가 일치하지 않습니다.');
     }
@@ -26,26 +23,20 @@ const signInHandler = async ({ socket, payload }) => {
 
     // 3. 소켓을 이용해서 유저 찾기
     const user = userSession.getUser(socket);
-
     if (!user) {
       throw new CustomError('소켓을 찾을 수 없습니다.');
     }
 
     // 4. 중복 로그인 유저 세션에서 체크
-    const existingUser = userSession.forEach((user, key, map) => {
-      if (user.email === email) return user;
-    });
-
+    const existingUser = userSession.checkId(email);
     if (existingUser) {
       throw new CustomError('이미 접속 중인 유저입니다.');
     }
-
     // 5. 찾은 유저에 로그인 정보 추가
-    user.login(newId, userData.name); // TODO 나중에 newId -> email로 변경하기
+    user.login(userData.id, userData.email, userData.name); // TODO 나중에 newId -> email로 변경하기
+    userSession.addId(email, socket);
 
-    newId += 1; // TODO 나중에 삭제
-
-    console.log(`${user.name} 님이 로그인하였습니다.`);
+    // console.log(`${user.name} 님이 로그인하였습니다.`);
 
     // 6. 패킷 전송
     const loginResponse = makePacket(PACKET_TYPE.LOGIN_RESPONSE, {
