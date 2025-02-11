3c3d00b05070ba2fd50cc36a3a7c1731932b9eae
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-02-11 14:45:57 +0900

    add: 임시 연결종료 로직 추가

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -1,5 +1,15 @@
+import { userSession } from "../sessions/session";
+
 const onEnd = (socket) => () => {
   console.log('클라이언트 연결이 종료되었습니다.');
+
+  // 세션에서 제거
+  userSession.deleteEmail(socket);
+  userSession.deleteUser(socket);
+
+  // 진짜 종료 시켜주기
+  socket.end();
+  socket.destroy();
 };
 
 export default onEnd;
