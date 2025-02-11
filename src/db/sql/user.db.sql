4243c41034167a13983f436ced06324fc11cfdeb
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-02-11 14:35:31 +0900

    fix: db 컬럼이름 변경
    user_id => id
    및 쿼리에서 사용하지 않는 select 컬럼 삭제

--- src/db/sql/user.db.sql
+++ src/db/sql/user.db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    user_id         INT AUTO_INCREMENT PRIMARY KEY,
+    id              INT AUTO_INCREMENT PRIMARY KEY,
     name            VARCHAR(255) UNIQUE,
     email           VARCHAR(255) UNIQUE,
     password        VARCHAR(255),
