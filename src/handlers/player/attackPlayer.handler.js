1cbe3227825b87b57c5e63b2cf7359afe6fde36a
EuiJoong <euijki87@gmail.com>
2025-02-11 15:56:33 +0900

    fix: 몬스터 유효타 로직 제거(기능 세분화)

--- src/handlers/player/attackPlayer.handler.js
+++ src/handlers/player/attackPlayer.handler.js
@@ -40,64 +40,6 @@ const attackPlayerHandler = ({ socket, payload }) => {
   const motionPayload = { playerId: player.id };
   const packet = makePacket(PACKET_TYPE.PLAYER_ATTACK_NOTIFICATION, motionPayload);
   game.notification(socket, packet);
-
-  // 플레이어 위치 조회
-  const { x: playerX, y: playerY } = player.getPlayerPos();
-
-  // 몬스터 목록 조회
-  const monsterList = game.getAllMonster();
-
-  // 몬스터 리스트 순회
-  monsterList.forEach((monster) => {
-    // 몬스터 정보 조회
-    const { id: monsterId, x: monsterX, y: monsterY } = monster.monsterDataSend();
-
-    //대상(몬스터)의 거리 계산
-    const distance = calculateDistance(playerX, playerY, monsterX, monsterY);
-
-    // 공격 범위 내에 몬스터가 있으면
-    if (distance <= player.getRange()) {
-      // 각도 계산
-      const angleToMonster = calculateAngle(
-        playerX,
-        playerY,
-        playerDirX,
-        playerDirY,
-        monsterX,
-        monsterY,
-      );
-
-      // 공격 각도 내에 몬스터가 있으면
-      if (angleToMonster <= player.getAngle() / 2) {
-        // TODO : 테스트 로그
-        console.log(`MONSTER ID: ${monster.getMonsterId()} (${monsterX}, ${monsterY}) ATTACK`);
-
-        // 몬스터 HP 차감 처리
-        const damege = player.getPlayerAtkDamage();
-        const currHp = monster.setDamaged(damege);
-
-        if (currHp <= 0) {
-          // 몬스터 사망 처리
-          game.removeMonster(monsterId);
-        }
-
-        // 패킷 생성
-        const packet = makePacket(PACKET_TYPE.MONSTER_HP_UPDATE_NOTIFICATION, {
-          monsterId,
-          damege,
-        });
-
-        // broadcast - 모든 플레이어들에게 전달
-        game.broadcast(packet);
-      } else {
-        // TODO : 테스트 로그
-        console.log(`MONSTER ID: ${monster.getMonsterId()} (${monsterX}, ${monsterY}) ANGLE OUT`);
-      }
-    } else {
-      // TODO : 테스트 로그
-      console.log(`MONSTER ID: ${monster.getMonsterId()} (${monsterX}, ${monsterY}) RANGE OUT`);
-    }
-  });
 };
 
 export default attackPlayerHandler;
