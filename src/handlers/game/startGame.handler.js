ba6bb9f1bdb642f471bbbce4ad5724c1012ba34f
EuiJoong <euijki87@gmail.com>
2025-02-12 12:34:43 +0900

    update: 몬스터 좌표 값 변경 로직 추가

--- src/handlers/game/startGame.handler.js
+++ src/handlers/game/startGame.handler.js
@@ -2,26 +2,36 @@ import { config } from '../../config/config.js';
 import { roomSession, userSession } from '../../sessions/session.js';
 import makePacket from '../../utils/packet/makePacket.js';
 
-const gameStartHandler = ({socket, payload}) => {
+const gameStartHandler = ({ socket, payload }) => {
   try {
+    const { monsters, objects } = payload;
+
     const user = userSession.getUser(socket);
     const room = roomSession.getRoom(user.roomId);
 
-    if(!room) {
+    if (!room) {
       throw new Error('방 생성에 실패했습니다!');
     }
-    
+
+    const game = room.getGame();
+    if (!game) {
+      throw new Error('게임 생성에 실패했습니다!');
+    }
+
+    // 몬스터 : 클라에서 생성된 좌표 값으로 변경
+    monsters.forEach((monster) => {
+      game.updateMonsterPosition(monster.monsterId, monster.x, monster.y);
+    });
+
     const GameStartNotification = makePacket(config.packetType.START_GAME_NOTIFICATION, {
       gameState: { phaseType: 1, nextPhaseAt: 100000 }, //이삭님 코드에 이렇게돼있음!
       users: room.getUserData(),
-      characterPositions: room.getUsersPositionData()
-    })
+      characterPositions: room.getUsersPositionData(),
+    });
 
     room.startGame();
     room.broadcast(GameStartNotification);
-  } catch (err) {
-
-  }
+  } catch (err) {}
 };
 
 export default gameStartHandler;
