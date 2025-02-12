e25eda673fba5ee800ca0846568cb246ab284115
yoon-H <ucando0320@gmail.com>
2025-02-12 21:22:17 +0900

    move: move enum to constants

--- src/config/constants/game.js
+++ src/config/constants/game.js
@@ -1,2 +1,14 @@
+export const DayPhase = {
+  DAY: 0,
+  NIGHT: 1,
+};
+Object.freeze(DayPhase);
+
+export const WaveState = {
+  NONE: 0,
+  INWAVE: 1,
+};
+Object.freeze(WaveState);
+
 export const DAY_TIME = 180000;
 export const NIGHT_TIME = 120000;
