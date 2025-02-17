0e8ca79545418bd164fb5e29d83bb77ba64436e3
EuiJoong <euijki87@gmail.com>
2025-02-17 21:34:22 +0900

    fix: 로비,게임 서버별 포트 수정

--- src_lobby/config/constants/env.js
+++ src_lobby/config/constants/env.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 5555;
+export const PORT = process.env.PORT_LOBBY || 5556;
 export const HOST = process.env.HOST || 'localhost';
 export const CLIENT_VERSION = process.env.CLIENT_VERSION || '1.0.0';
 // 클라에서 0.2초마다 이동요청 패킷을 보냄
