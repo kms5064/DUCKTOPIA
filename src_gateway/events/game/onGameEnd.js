25e73aac7d0d3a8b3649c38fd73a283f35d1c50c
EuiJoong <euijki87@gmail.com>
2025-02-18 20:31:58 +0900

    add: 게임서버,로비서버 소켓 연결 파일 추가

---
+++ src_gateway/events/game/onGameEnd.js
@@ -0,0 +1,19 @@
+import { userSession } from '../../sessions/session.js';
+import CustomError from '../../utils/error/customError.js';
+
+const onGameEnd = (socket) => () => {
+  console.log('클라이언트 연결이 종료되었습니다.');
+
+  // 1. 게임 내에 있으면 제거
+  const user = userSession.getUser(socket.id);
+  if (!user) throw new CustomError('유저가 없습니다!');
+
+  // 세션에서 제거
+  userSession.deleteUser(socket.id);
+
+  // 진짜 종료 시켜주기
+  socket.end();
+  socket.destroy();
+};
+
+export default onGameEnd;
