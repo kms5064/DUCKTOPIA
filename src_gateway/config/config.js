3768c79ad262f84c93c2a6e0adcf6978d3964228
EuiJoong <euijki87@gmail.com>
2025-02-18 16:40:39 +0900

    add: 게이트웨이 서버 파일 추가

---
+++ src_gateway/config/config.js
@@ -0,0 +1,45 @@
+import {
+  CLIENT_VERSION,
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+  HOST,
+  PORT,
+  LOCATION_REQ_TIME_TERM,
+} from './constants/env.js';
+import {
+  PACKET_TYPE,
+  PACKET_TYPE_BYTE,
+  PAYLOAD_LENGTH_BYTE,
+  VERSION_LENGTH_BYTE,
+} from './constants/header.js';
+
+export const config = {
+  header: {
+    packetTypeByte: PACKET_TYPE_BYTE,
+    versionLengthByte: VERSION_LENGTH_BYTE,
+    payloadLengthByte: PAYLOAD_LENGTH_BYTE,
+  },
+  packetType: {
+    ...PACKET_TYPE,
+  },
+  server: {
+    host: HOST,
+    port: PORT,
+  },
+  client: {
+    version: CLIENT_VERSION,
+    locationReqTimeTerm: LOCATION_REQ_TIME_TERM,
+  },
+  databases: {
+    USER_DB: {
+      name: DB_NAME,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      host: DB_HOST,
+      port: DB_PORT,
+    },
+  },
+};
