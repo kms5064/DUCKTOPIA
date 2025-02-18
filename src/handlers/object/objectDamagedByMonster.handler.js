390820f377b6929e7247c3ef66ea3829e3802d31
이이삭 <dlrltn1091@gmail.com>
2025-02-18 14:30:12 +0900

    fix: bug

--- src/handlers/object/objectDamagedByMonster.handler.js
+++ src/handlers/object/objectDamagedByMonster.handler.js
@@ -16,15 +16,19 @@ const objectDamagedByMonsterHandler = async ({ socket, payload }) => {
   if (objectId === 1) {
     console.log(`코어가 ${monster.getAttack()}의 데미지를 받았습니다!`);
     const CoreHp = game.coreDamaged(monster.getAttack());
-    const payload = { objectId: objectId, hp: CoreHp };
-    packet = makePacket(config.packetType.S_OBJECT_HP_UPDATE_NOTIFICATION, payload);
-    game.broadcast(packet);
-    if (CoreHp <= 0) {
+    if (CoreHp <= 0) {//게임중일때만 보냄.
+      //1.룸 상태를 게임 끝난거(대기)로 고침
       console.log(`코어가 파괴되었습니다. HP: ${CoreHp}`);
       const gameOverPayload = {};
       gameOverPacket = makePacket(config.packetType.S_GAME_OVER_NOTIFICATION, gameOverPayload);
       game.broadcast(gameOverPacket);
+  
+    }else{
+    const payload = { objectId: objectId, hp: CoreHp };
+    packet = makePacket(config.packetType.S_OBJECT_HP_UPDATE_NOTIFICATION, payload);
+    game.broadcast(packet);
     }
+
   } else {
   }
 };
