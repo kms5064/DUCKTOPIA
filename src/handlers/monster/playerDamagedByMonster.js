c1e5eaa6c8c5fb245d9843986f7b53a43e781d62
kimjehoon <kms5064@naver.com>
2025-02-19 20:40:11 +0900

    fix: 예외처리

--- src/handlers/monster/playerDamagedByMonster.js
+++ src/handlers/monster/playerDamagedByMonster.js
@@ -14,23 +14,24 @@ const playerDamagedByMonsterHandler = async ({ socket, payload }) => {
 
   let packet;
   let monsterAttackPayload;
+  if (monster && player) {
+    const remainPlayerHp = player.changePlayerHp(monster.getAttack());
+    //몬스터가 플레이어를 때렸을 때 [3] : 충격 처리
+    if (remainPlayerHp <= 0) {
+      //유저 사망 처리 먼저 하도록 하자.
+      //플레이어가 살아날 위치를 지정해준다.
+      console.log('플레이어 사망');
+      const deadPayload = { playerId: playerId };
+      packet = makePacket(config.packetType.S_PLAYER_DEATH_NOTIFICATION, deadPayload);
+    } else {
+      //동기화 패킷은 계속 보내지고 있을 터이니 그 쪽에서 처리하면 될 테고
 
-  const remainPlayerHp = player.changePlayerHp(monster.getAttack());
-  //몬스터가 플레이어를 때렸을 때 [3] : 충격 처리
-  if (remainPlayerHp <= 0) {
-    //유저 사망 처리 먼저 하도록 하자.
-    //플레이어가 살아날 위치를 지정해준다.
-    console.log('플레이어 사망');
-    const deadPayload = { playerId: playerId };
-    packet = makePacket(config.packetType.S_PLAYER_DEATH_NOTIFICATION, deadPayload);
-  } else {
-    //동기화 패킷은 계속 보내지고 있을 터이니 그 쪽에서 처리하면 될 테고
+      monsterAttackPayload = { playerId: playerId, hp: player.hp };
+      packet = makePacket(config.packetType.S_PLAYER_HP_UPDATE_NOTIFICATION, monsterAttackPayload);
+    }
 
-    monsterAttackPayload = { playerId: playerId, hp: player.hp };
-    packet = makePacket(config.packetType.S_PLAYER_HP_UPDATE_NOTIFICATION, monsterAttackPayload);
+    game.broadcast(packet);
   }
-
-  game.broadcast(packet);
 };
 
 export default playerDamagedByMonsterHandler;
