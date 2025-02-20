b3ac9b0690682a0af1ca0d3f280bd35eff758bb6
leejunsung <junstar96@naver.com>
2025-02-20 14:19:39 +0900

    fix : 보스몬스터 수정

--- src/classes/game/bossMonster.class.js
+++ src/classes/game/bossMonster.class.js
@@ -23,16 +23,77 @@ class bossMonster extends Monster {
 
     }
 
-    cencelPattern() {
+    cancelPattern() {
         if (this.bossPatternTimeOut !== null) {
             clearTimeout(this.bossPatternTimeOut);
             this.bossPatternTimeOut = null;
         }
     }
 
+    //기존에는 패턴이 종료되었을 때 
+    lostPlayer() {
+    }
+
+    setPattern() {
+        if (this.bossPatternTimeOut !== null) {
+            return;
+        }
+        const patternType = Math.floor(Math.random() * 10);
+        switch (patternType) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+                this.bossPatternTimeOut = setTimeout(() => {
+                    //보스 몬스터 내부에는 이런 식으로 
+                    this.cancelPattern();
+                }, 1000);
+                break;
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+                this.bossPatternTimeOut = setTimeout(() => {
+
+                }, 4000);
+                break;
+            case 8:
+            case 9:
+                this.bossPatternTimeOut = setTimeout(() => {
+
+                }, 5000);
+                break;
+            default:
+                break;
+        }
+    }
+
+    dropItemCount() {
+        const defaultdrop = super.dropItemCount() + 3;
+        return defaultdrop;
+    }
+
+
     death() {
         super.death();
     }
 
+    setTargetPlayer(player) {
+        super.setTargetPlayer(player);
+        this.setPattern();
+    }
+
+
+
+    lostPlayer() {
+        if (this.targetPlayer === null) {
+            return;
+        }
+
+
+    }
+
 
 }
+
+export default bossMonster;
