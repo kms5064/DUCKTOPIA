5802010dc39f814d1966099f17436cd4bd505c61
EuiJoong <euijki87@gmail.com>
2025-02-20 20:18:39 +0900

    fix: 포트 주석

--- 3.src_game/config/constants/env.js
+++ 3.src_game/config/constants/env.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// export const PORT = process.env.PORT || 5558;
 export const PORT = 5558;
 export const HOST = process.env.HOST || '127.0.0.1';
 export const CLIENT_VERSION = process.env.CLIENT_VERSION || '1.0.0';
