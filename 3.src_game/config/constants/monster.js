6be384f0f1fba430228a7e2471e7a06604711762
EuiJoong <euijki87@gmail.com>
2025-02-21 19:49:01 +0900

    test: 테스트코드

--- 3.src_game/config/constants/monster.js
+++ 3.src_game/config/constants/monster.js
@@ -1,6 +1,6 @@
 //드랍률 테이블에 쓰일 숫자. 이 값을 베이스로 log(x)/log(2)로 연산하여 아이템 드롭 갯수 테이블을 작성할 예정
 export const MAX_DROP_ITEM_COUNT = 1024;
-export const MAX_SPAWN_COUNT = 1000;
+export const MAX_SPAWN_COUNT = 500;
 
 export const NORMAL_MONSTER_MAX_CODE = 8;
 
