3c4c2c4765c9d3d62d5d76e79a73095f8e819e4c
ssini-oh <ssini8280@gmail.com>
2025-02-25 17:15:45 +0900

    update: 몬스터 처치 후 아이템 습득 로직 수정 및 테스트

--- src/classes/item/item.class.js
+++ src/classes/item/item.class.js
@@ -2,8 +2,7 @@
 // 아이템 상태, 아이템 관련 기본 동작 등 하나의 아이템 인스턴스만을 관리함
 
 class Item {
-  constructor({ type, itemData, position }) {
-    this.type = type; // 아이템 타입 (FOOD, WEAPON)
+  constructor({ itemData, position }) {
     this.itemData = itemData; // { itemCode: number, count: number }
     this.position = position; // 아이템 위치
     this.isPickedUp = false; // 습득 여부
@@ -22,13 +21,6 @@ class Item {
     this.isPickedUp = true;
     return true;
   }
-
-  // 아이템 타입 열거형
-  static Type = {
-    UNKNOWN: 0,
-    FOOD: 1,
-    WEAPON: 2,
-  };
 }
 
 export default Item;
