d1d2ad030943c36506ea2ebbd56dfdc33e72eeb0
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-02-27 21:20:19 +0900

    add: 클라이언트 Latency 적용 추가

--- 1.src_gateway/handlers/server/latencyCheck.handler.js
+++ 1.src_gateway/handlers/server/latencyCheck.handler.js
@@ -1,10 +1,17 @@
+import { config } from "../../config/config.js";
+import makePacket from "../../utils/packet/makePacket.js";
+
 const latencyCheckHandler = ({ socket, payload, userId }) => {
-  if (userId !== -1) return;
-  const { errorMessage, timestamp } = payload;
-  const latency = Date.now() - timestamp;
-  console.log(
-    `###Latency### ${errorMessage} 와의 총 왕복 시간${latency}ms / 평균 Latency ${latency / 2}ms`,
-  );
+  if (userId !== -1) {
+    const packet = makePacket(config.packetType.S_ERROR_NOTIFICATION, payload)
+    socket.write(packet)
+  } else {
+    const { errorMessage, timestamp } = payload;
+    const latency = Date.now() - timestamp;
+    console.log(
+      `###Latency### ${errorMessage} 와의 총 왕복 시간${latency}ms / 평균 Latency ${latency / 2}ms`,
+    );
+  }
 };
 
 export default latencyCheckHandler;
