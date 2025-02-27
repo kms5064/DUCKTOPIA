04d29f15217cebd4386e39496107fa38b64c0c4b
EuiJoong <euijki87@gmail.com>
2025-02-27 14:29:25 +0900

    delete: 미사용 파일 삭제

--- 0.src copy/config/constants/itemBox.js
+++
@@ -1,2 +0,0 @@
-export const VALID_DISTANCE_OF_BOX = 2;
-export const ITEM_BOX_HP = 100;
--- 0.src copy/sessions/session.js
+++
@@ -1,5 +0,0 @@
-import UserSession from '../classes/user/userSession.class.js';
-import RoomSession from '../classes/room/roomSession.class.js';
-
-export const userSession = new UserSession();
-export const roomSession = new RoomSession();
