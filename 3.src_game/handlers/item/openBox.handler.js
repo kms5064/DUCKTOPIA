d59dac36378a3fe16750143ac518c46003332e7a
EuiJoong <euijki87@gmail.com>
2025-03-04 17:55:07 +0900

    update: 코어 박스 로직 추가

--- 3.src_game/handlers/item/openBox.handler.js
+++ 3.src_game/handlers/item/openBox.handler.js
@@ -14,31 +14,48 @@ const playerOpenBoxHandler = ({ socket, payload, userId }) => {
   const game = gameSession.getGame(user.getGameId());
   if (!game) throw new CustomError(`Game ID(${user.getGameId()}): Game 정보가 없습니다.`);
 
-  const itemBox = game.getItemBoxById(itemBoxId);
-  if (!itemBox) throw new CustomError(`itemBoxId : ${itemBoxId} 를 찾을 수 없습니다.`);
+  let itemList = [];
+  if (config.game.itemBox.objectCoreCode === itemBoxId) {
+    const core = game.getCore();
+    if (core.occupied === null) {
+      core.occupied = userId;
 
-  //유효한 거리인지 검증
-  //이 박스가 점유중이라면 컷
-  //박스 오픈한채로 돌아다니면 박스 닫히게?
-  if (
-    //itemBox.calculateDistance <= config.game.VALID_DISTANCE_OF_BOX &&
-    itemBox.occupied === null
-  ) {
-    itemBox.occupied = userId;
+      itemList = core.getItemList();
+      const payload = {
+        playerId: userId,
+        itemBoxId: itemBoxId,
+        itemData: itemList,
+      };
 
-    const itemList = [];
-    itemBox.getItemList();
-    const payload = {
-      playerId: userId,
-      itemBoxId: itemBoxId,
-      itemData: itemList,
-    };
+      const notification = [config.packetType.S_PLAYER_OPEN_BOX_NOTIFICATION, payload];
+      game.broadcast(notification);
+      console.log(`itemBoxId : ${itemBoxId}를 열었다!`);
+    }
+  } else {
+    const itemBox = game.getItemBoxById(itemBoxId);
+    if (!itemBox) throw new CustomError(`itemBoxId : ${itemBoxId} 를 찾을 수 없습니다.`);
 
-    const notification = [config.packetType.S_PLAYER_OPEN_BOX_NOTIFICATION, payload];
-    //이 유저가 열고 있다는거 브로드캐스트
+    //유효한 거리인지 검증
+    //이 박스가 점유중이라면 컷
+    //박스 오픈한채로 돌아다니면 박스 닫히게?
+    if (
+      //itemBox.calculateDistance <= config.game.VALID_DISTANCE_OF_BOX &&
+      itemBox.occupied === null
+    ) {
+      itemBox.occupied = userId;
+      itemBox.getItemList();
+      const payload = {
+        playerId: userId,
+        itemBoxId: itemBoxId,
+        itemData: itemList,
+      };
 
-    game.broadcast(notification);
-    console.log(`itemBoxId : ${itemBoxId}를 열었다!`);
+      const notification = [config.packetType.S_PLAYER_OPEN_BOX_NOTIFICATION, payload];
+      //이 유저가 열고 있다는거 브로드캐스트
+
+      game.broadcast(notification);
+      console.log(`itemBoxId : ${itemBoxId}를 열었다!`);
+    }
   }
 };
 
