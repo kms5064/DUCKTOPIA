c569a65bbb7c9cae4310a78fd3c07e460fe9e6b6
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-03-04 16:38:20 +0900

    bug-fix: id로 바뀐 부분 수정

--- 1.src_gateway/events/lobby/onLobbyError.js
+++ 1.src_gateway/events/lobby/onLobbyError.js
@@ -7,7 +7,7 @@ const onLobbyError = (socket) => async (err) => {
   serverSession.deleteServer(socket.id);
 
   await redisClient.hSet(socket.id, 'status', 0);
-  await redisClient.publish('ServerOff', socket.name);
+  await redisClient.publish('ServerOff', socket.id);
 
   // 진짜 종료 시켜주기
   socket.end();
