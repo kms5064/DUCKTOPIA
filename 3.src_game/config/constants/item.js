35bd036db949894b0674b5e2f1e50d122ff7257a
EuiJoong <euijki87@gmail.com>
2025-03-04 12:01:06 +0900

    update: 머스타드 제작 재료 코드 추가

--- 3.src_game/config/constants/item.js
+++ 3.src_game/config/constants/item.js
@@ -1,2 +1,6 @@
 // 아이템 습득 관련 상수
 export const ITEM_PICKUP_RANGE = 2; // 아이템 습득 가능 거리 (유닛)
+
+export const MUSTAD_MATERIAL_CODE1 = 7; //꿀
+export const MUSTAD_MATERIAL_CODE2 = 8; //계란 후라이
+export const MUSTAD_MATERIAL_CODE3 = 801; //머스타드 씨앗
