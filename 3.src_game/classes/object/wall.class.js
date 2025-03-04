adf9df9e11efe0d47e28de25feb132c75524a310
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-03-04 18:09:03 +0900

    bug-fix: 참조오류 수정

--- 3.src_game/classes/object/wall.class.js
+++ 3.src_game/classes/object/wall.class.js
@@ -1,10 +1,9 @@
 import DestructibleObjectBase from '../base/destructibleObjectBase.class.js';
 import { getGameAssets } from '../../init/assets.js';
 
-const { objectDropTable }= getGameAssets()
-
 class Wall extends DestructibleObjectBase {
     constructor(id, objectCode, x, y) {
+        const { objectDropTable } = getGameAssets()
         const { name, maxHp } = objectDropTable.data.find((e) => e.objectCode === objectCode);
         super(id, objectCode, name, maxHp, x, y);
     }
