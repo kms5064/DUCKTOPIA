b42acea795303f52913d5c0428c002369dc6f2d1
EuiJoong <euijki87@gmail.com>
2025-03-06 10:37:16 +0900

    update: 장비 합성 기능 추가

--- 3.src_game/handlers/item/equipmentUpgrade.handler.js
+++ 3.src_game/handlers/item/equipmentUpgrade.handler.js
@@ -4,10 +4,7 @@ import { getGameAssets } from '../../init/assets.js';
 import { config } from '../../config/config.js';
 
 const equipmentUpgradeHandler = ({ socket, payload, userId }) => {
-  console.log('## weaponGradeupHandler');
   const { itemCode1, itemCode2 } = payload;
-
-  console.log('## itemCode1', itemCode1, '## itemCode2 ', itemCode2);
 
   // 데이터 체크
   if (!itemCode1 || !itemCode2) {
@@ -44,10 +41,7 @@ const equipmentUpgradeHandler = ({ socket, payload, userId }) => {
   const itemGrades = [];
 
   // 에셋 데이터 검증
-
   items.forEach((item) => {
-    console.log('foreach');
-    console.log(item);
     let data = null;
     /**
      * 101 ~ 120 무기 , 301 ~ 305 갑옷
@@ -79,51 +73,39 @@ const equipmentUpgradeHandler = ({ socket, payload, userId }) => {
     if (!data) throw new CustomError('존재하지않는 아이템 입니다.');
 
     itemDatas.push(data);
-    // const idx = itemGrades.findIndex((grade) => grade === item.grade);
-    console.log(config.game.item.equipmentGrades);
-    console.log(config.game.item.equipmentGrades.findIndex((grade) => grade === data.grade));
     itemGrades.push(config.game.item.equipmentGrades.findIndex((grade) => grade === data.grade));
   });
 
-  console.log('@@@@@');
-  console.log('itemData : ', itemDatas);
-  console.log('grades : ', itemGrades);
-  /**
-   * 동일 등급 : 성공률 100%
-   */
   let isSuccess = false;
   let createGrade = null;
   if (itemGrades[0] !== itemGrades[1]) {
-    // TODO : 체크
+    /***********
+     * 다른 등급
+     */
     // 가중치 합산
     const totalWeight =
       upgradeRate.data[config.game.item.equipmentGrades[itemGrades[0]]] +
       upgradeRate.data[config.game.item.equipmentGrades[itemGrades[1]]];
     // 확률 처리
     const random = Math.floor(Math.random() * 100) + 1;
-    console.log('totalWeight : ', totalWeight);
-    console.log('random: ', random);
 
     const maxIndex = Math.max(itemGrades[0], itemGrades[1]);
     // 성공 실패 여부
     if (random <= totalWeight) {
       // 성공
       isSuccess = true;
-
       createGrade =
-        config.game.item.equipmentGrades.length - 1 < maxIndex
+        config.game.item.equipmentGrades.length - 1 > maxIndex
           ? config.game.item.equipmentGrades[maxIndex + 1]
           : config.game.item.equipmentGrades[maxIndex];
     } else {
       //실패
       createGrade = config.game.item.equipmentGrades[maxIndex];
     }
-    // upgradeRate[grade];
   } else {
-    /**
-     * 동일 등급 합성 처리
+    /***********
+     * 동일 등급
      */
-
     isSuccess = true;
     createGrade =
       config.game.item.equipmentGrades.length - 1 === itemGrades[0]
@@ -131,13 +113,8 @@ const equipmentUpgradeHandler = ({ socket, payload, userId }) => {
         : config.game.item.equipmentGrades[itemGrades[0] + 1];
   }
 
-  console.log('isSuccess : ', isSuccess);
-  console.log('createGrade : ', createGrade);
-
   // 등급에 따른 아이템 생성
-
   let equipmentList = null;
-  console.log('itemType : ', itemType);
   switch (itemType) {
     case '1':
       equipmentList = weapon.data.filter((data) => data.grade === createGrade);
@@ -167,7 +144,10 @@ const equipmentUpgradeHandler = ({ socket, payload, userId }) => {
 
   const createRandom = Math.floor(Math.random() * equipmentList.length);
   const createEquipment = equipmentList[createRandom];
-  console.log('조합에 성공하여 새로운 아이템이 생성되었습니다.', createEquipment);
+  console.log(
+    `조합에 ${isSuccess ? '성공' : '실패'} 하여 ${createGrade} 등급의 새로운 아이템이 생성되었습니다.`,
+    createEquipment,
+  );
 
   // 유저 인벤토리에 아이템 생성
   user.player.addItem(createEquipment.code, 1, emptyIndex);
@@ -179,8 +159,6 @@ const equipmentUpgradeHandler = ({ socket, payload, userId }) => {
     itemResultCode: createEquipment.code,
     playerId: userId,
   };
-
-  console.log(sendPayload);
 
   const packet = [config.packetType.S_ITEM_COMBINATION_NOTIFICATION, sendPayload];
   game.broadcast(packet);
