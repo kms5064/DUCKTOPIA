20134a5bc8fb16fdb2d581a3e922efcb9beb50d9
kimjehoon <kms5064@naver.com>
2025-03-07 12:21:44 +0900

    fix:버그픽스
    1. 코어 안때려지는 버그
    2. 아이템박스 고장 버그

--- 3.src_game/handlers/object/objectDamagedByMonster.handler.js
+++ 3.src_game/handlers/object/objectDamagedByMonster.handler.js
@@ -15,22 +15,28 @@ const objectDamagedByMonsterHandler = async ({ socket, payload, userId }) => {
   const monster = game.getMonsterById(monsterId);
   if (!monster) throw new CustomError(`Monster ID : ${monsterId}는 존재하지 않습니다.`);
 
-  const object = game.objects.get(objectId);
-  if (!object) throw new CustomError('오브젝트를 찾을 수 없습니다');
-
   // 오브젝트에 따라 다르게 적용
   let sendPayload = null;
+
   switch (objectId) {
     case 1:
-      const coreHp = game.coreDamaged(monster.getAttack());
+      const coreHp = game.core.coreDamaged(monster.getAttack());
       // console.log(`코어가 ${monster.getAttack()}의 데미지를 받았습니다! HP: ${coreHp}`);
       sendPayload = { objectId: objectId, hp: coreHp };
+      if(coreHp <= 0) {
+        game.gameEnd();
+      }
       break;
     default:
-      const objectHp = object.changeObjectHp(monster.getAttack())
-      sendPayload = { objectId: objectId, hp: objectHp }
-      if (objectHp > 0) break
-      const objectDestroyNotification = [config.packetType.S_OBJECT_DESTROY_NOTIFICATION, {objectId}];
+      const object = game.objects.get(objectId);
+      if (!object) throw new CustomError('오브젝트를 찾을 수 없습니다');
+      const objectHp = object.changeObjectHp(monster.getAttack());
+      sendPayload = { objectId: objectId, hp: objectHp };
+      if (objectHp > 0) break;
+      const objectDestroyNotification = [
+        config.packetType.S_OBJECT_DESTROY_NOTIFICATION,
+        { objectId },
+      ];
       game.broadcast(objectDestroyNotification);
       return;
   }
