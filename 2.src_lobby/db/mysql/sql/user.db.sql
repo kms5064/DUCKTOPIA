14026b597815461e24cc8a842bba16f63dd32bb2
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-03-11 12:26:52 +0900

    delete: 미사용 폴더/파일 삭제

--- 2.src_lobby/db/mysql/sql/user.db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users
-(
-    id              INT AUTO_INCREMENT PRIMARY KEY,
-    name            VARCHAR(255) UNIQUE,
-    email           VARCHAR(255) UNIQUE,
-    password        VARCHAR(255),
-    create_dt       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    update_dt       TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
