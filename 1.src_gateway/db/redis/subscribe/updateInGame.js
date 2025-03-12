b07a7c3b4123ff31e900bc343d3884c69caad2e4
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-03-12 20:15:13 +0900

    bug-fix: 유저 Redis 동기화 오류 수정
    기존에 Redis에 있는 User가 업데이트 되지 않는 버그 수정

--- 1.src_gateway/db/redis/subscribe/updateInGame.js
+++ 1.src_gateway/db/redis/subscribe/updateInGame.js
@@ -1,12 +1,16 @@
+import { config } from '../../../config/config.js';
 import { userSession } from '../../../sessions/session.js';
+import { redisClient } from '../redis.js';
 
-const updateInGame = (userIds) => {
+const updateInGame = async (userIds) => {
   const users = userIds.split(',');
 
   for (const userId of users) {
     const user = userSession.getUserByID(+userId);
     if (!user) continue;
     user.setGameState(false);
+    // Redis 정보 동기화
+    await redisClient.hDel(config.redis.custom + 'Server:User' + user.id, 'game')
   }
 };
 
